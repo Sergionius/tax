3838bbbe5a732ec8f8b6e102a2295977e1783b72
Sergio Malkin <serg-eagle@outlook.com>
2026-07-15 13:43:23 +0300

    added xcode project

---
+++ ios/tax/tax/taxApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxApp.swift
+//  tax
+//
+//  Created by Sergio Malkin on 15.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct taxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
